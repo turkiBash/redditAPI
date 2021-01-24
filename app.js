239a1c2066fa94c114a4f5f37dbb93fd52a900ee
Turki <dev@turki.tech>
2021-01-24 14:41:57 +0300

    adding <a> tag

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 fetch("https://www.reddit.com/r/Overwatch/.json")
   .then((response) => response.json())
+
   .then((data) => {
     console.log(data);
+
     const threads = data.data.children;
+
     for (var i = 0; i < threads.length; i++) {
-      //console.log(threads[i].data);
+      console.log(threads[i].data);
+
       console.log(threads[i].data.title);
-      //let html = `<ol>${threads.title}</ol>`;
-      var html1 = document.createElement("p", "a");
-      var node = document.createTextNode(
-        threads[i].data.title,
-        threads[i].data.url
-      );
+
+      console.log(threads[i].data.url);
+
+      const html1 = document.createElement("a");
+
+      html1.setAttribute("href", threads[i].data.url);
+
+      const node = document.createTextNode(threads[i].data.title);
+
       html1.appendChild(node);
-      var element = document.getElementById("thread-list");
-      element.appendChild(html1);
+
+      const element1 = document.getElementById("thread-list");
+
+      element1.appendChild(html1);
     }
   });
